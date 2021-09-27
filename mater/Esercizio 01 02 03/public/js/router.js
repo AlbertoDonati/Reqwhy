a2f03d6f235c25e034735d6b719a65472e615f05
albertodonati97 <albertodonati97gmail.com>
2021-09-27 13:40:55 +0000

    esempio commit roba prof. es 6

---
+++ mater/Esercizio 01 02 03/public/js/router.js
@@ -0,0 +1,10 @@
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+      { path: '/', component: Home },
+      { path: '/movies', component: Movies },
+      { path: '/404', component: NotFound },  
+      { path: '*', redirect: '/404' }
+    ]
+  })
+  
