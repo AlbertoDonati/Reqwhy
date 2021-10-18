010f29dbda1f83f481e78ba2207accda8b2c2a4d
lubuntu <alberto.donati6@studio.unibo.it>
2021-10-18 16:09:46 +0200

    a posto login ecc buttons

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,23 +3,32 @@ const Navbar = {
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
           <div id="navbarNav">
             <ul class="navbar-nav">
-                <h2 style="text-emphasis-style: circle">Reqwhy</h2>
+                <a class="navbar-brand" href="/" style="text-emphasis-style: circle;color: darkblue;margin-left: 10pt">Reqwhy</a>
                 
-                <li><router-link class="nav-link" :to="'/'">HOME</router-link></li>
-                <li><router-link class="nav-link" :to="'/login'">LOGIN</router-link></li>
-                <li><router-link class="nav-link" :to="'/signup'">SIGNUP</router-link></li>
-                <li><router-link class="nav-link" :to="'/questions'" style="color: #3aa6d9">All</router-link></li>
-                <li><router-link class="nav-link" :to="'/crudquestions'">Insert</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/'">Home</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/questions'">All</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/crudquestions'">Insert</router-link></li>
                 
-                <li><router-link class="nav-link" :to="'/area/' + 'IT'">IT</router-link></li>
-                <li><router-link class="nav-link" :to="'/area/' + 'MATH'">MATH</router-link></li>
-                <li><router-link class="nav-link" :to="'/area/' + 'SCIENCE'">SCIENCE</router-link></li>
-                <li><router-link class="nav-link" :to="'/area/' + 'HISTORY'">HISTORY</router-link></li>
-                <li><router-link class="nav-link" :to="'/area/' + 'ART'">ART</router-link></li>
-                <li><router-link class="nav-link" :to="'/area/' + 'OTHER'">OTHER</router-link></li>
-             
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'IT'">IT</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'MATH'">MATH</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'SCIENCE'">SCIENCE</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'HISTORY'">HISTORY</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'ART'">ART</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" :to="'/area/' + 'OTHER'">OTHER</router-link></li>
+               
+                <li class="nav-item">
+                <button @click="goToLogin()" type="button" class="btn btnsm">
+                     <i class="fas fa-user"></i>
+                </button>
+                </li>
+                
+                <li class="nav-item">
+                <button @click="goToSignUp()" type="button" class="btn btnsm">
+                     <i class="fas fa-user-plus"></i>
+                </button>
+                </li>
+                
             </ul>
-            
           </div>
         </nav>
         `,
@@ -28,7 +37,15 @@ const Navbar = {
         }
     },
     methods: {
+        goToLogin() {
+                router.push({path: `/login`});
+            },
+        goToSignUp() {
+                router.push({path: `/signup`});
+        },
     },
+
+
 }
 /* sti navbutton non servono ad un cavolo */
 /*
